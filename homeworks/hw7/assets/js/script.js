437d82299c8221e2c42731f62f90af4ba8c95ccc
Yifan Fang <qf360530610@gmail.com>
2023-04-03 18:20:28 -0700

    finished hw7

---
+++ homeworks/hw7/assets/js/script.js
@@ -0,0 +1,21 @@
+function averageThreeNumbers(a, b, c) {
+    let sum = a+b+c;
+    let avg = sum/3;
+    return avg;
+}
+
+function createSentence(num, noun) {
+    return "On average, a Berkeley student has " + num + " " + noun + "s.";
+}
+
+function getRandomNum(max) {
+    return Math.floor(Math.random() * max);
+}
+
+let x = getRandomNum(20);
+let y = getRandomNum(10);
+let z = getRandomNum(13);
+let avg = averageThreeNumbers(x, y, z);
+let sentence = createSentence(avg, 'panda');
+
+console.log(sentence);
