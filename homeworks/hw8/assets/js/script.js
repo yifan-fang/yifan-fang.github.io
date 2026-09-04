e764571e68f218c03ab6236648a7b2a7a3c0aad0
Dartmouth User <dartuser@DW6ZC64.kiewit.dartmouth.edu>
2026-09-04 13:24:35 -0400

    replace course materials with research projects

--- homeworks/hw8/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-function toggleSongs() {
-    let songSecondHalf = document.getElementById('songs-second-half')
-    songSecondHalf.classList.toggle('hidden');
-}
-
-document.getElementById("toggle-button").onclick = toggleSongs;
